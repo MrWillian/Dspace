a874ff1a1e2150a40fe274715e23af419f4b6b53
MrWilllian <willian.dev10@gmail.com>
2020-04-01 19:52:09 -0300

    docs: add LOCAL_APPS in settings.py

--- dspace/dspace/settings.py
+++ dspace/dspace/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+LOCAL_APPS = [
+    'api',
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
