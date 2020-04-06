5cce8b10bf5326152f44a2180c7cc7c7442b174e
MrWilllian <willian.dev10@gmail.com>
2020-04-06 10:54:46 -0300

    test: add method to get data user object

--- dspace/api/tests.py
+++ dspace/api/tests.py
@@ -1,4 +1,3 @@
-from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 from django.contrib.auth.models import User
@@ -6,16 +5,26 @@ from django.contrib.auth.models import User
 class UserTestCase(APITestCase):
 
   def test_create_user(self):
-    self.post_data = {
-      'username': 'test',
-      'email': 'test@gmail.com',
-      'password': '123456'
-    }
-
-    return self.client.post('/api/user/', self.post_data, format='json')
+    self.post_data = self.data_user_object()
+    response = self.client.post('/api/user/', self.post_data, format='json')
     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+  # def test_get_user(self):
+  #   self.post_data = self.data_user_object()
+
+  #   self.client.post('/api/user/', self.post_data, format='json')
+  #   response = self.client.get('/api/user/1')
+  #   self.assertEqual(response.status_code, status.HTTP_200_OK)
 
   def test_list_users(self):
     response = self.client.get('/api/user/')
     self.assertEqual(response.status_code, status.HTTP_200_OK)
    
+  
+  def data_user_object(self):
+    return {
+      'id': 1,
+      'username': 'test',
+      'email': 'test@gmail.com',
+      'password': '123456'
+    }
