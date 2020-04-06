bb4e81d7b100dae97fb8fa4b2d1db720f4f4f529
MrWilllian <willian.dev10@gmail.com>
2020-04-06 10:53:27 -0300

    refactor: add Address class with fields

--- dspace/api/models.py
+++ dspace/api/models.py
@@ -1,2 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+class Address(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  country = models.CharField(max_length=50)
+  state = models.CharField(max_length=40)
+  city = models.CharField(max_length=50)
+  place = models.CharField(max_length=60)
+  latitude = models.DecimalField(max_digits=22, decimal_places=16, blank=True, null=True)
+  longitude = models.DecimalField(max_digits=22, decimal_places=16, blank=True, null=True)
