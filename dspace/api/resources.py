005f64d5e4be4c3aac5751107054c20efd2b3dab
MrWilllian <willian.dev10@gmail.com>
2020-04-06 19:19:04 -0300

    refactor: add authorization in Resources

--- dspace/api/resources.py
+++ dspace/api/resources.py
@@ -1,15 +1,18 @@
-from tastypie.resources import ModelResource
 from django.contrib.auth.models import User
-from tastypie import fields
 from api.models import Address
+from tastypie.authorization import Authorization
+from tastypie.resources import ModelResource
+from tastypie import fields
 
 class UserResource(ModelResource):
   class Meta:
     queryset = User.objects.all()
     resource_name = 'user'
+    authorization = Authorization()
 
 class AddressResource(ModelResource):
   user = fields.ForeignKey(UserResource, 'user')
   class Meta:
     queryset = Address.objects.all()
     resource_name = 'address'
+    authorization = Authorization()
