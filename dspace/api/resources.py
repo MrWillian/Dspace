07f32a97e145952eb320e78ae99cbc6487417dd4
MrWilllian <willian.dev10@gmail.com>
2020-04-06 10:54:00 -0300

    refactor: add AddressResource

--- dspace/api/resources.py
+++ dspace/api/resources.py
@@ -1,7 +1,15 @@
 from tastypie.resources import ModelResource
 from django.contrib.auth.models import User
+from tastypie import fields
+from api.models import Address
 
 class UserResource(ModelResource):
   class Meta:
     queryset = User.objects.all()
     resource_name = 'user'
+
+class AddressResource(ModelResource):
+  user = fields.ForeignKey(UserResource, 'user')
+  class Meta:
+    queryset = Address.objects.all()
+    resource_name = 'Address'
