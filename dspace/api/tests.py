c43402e5d63e24aaebd2f05c7aaa6da2bc029b32
MrWilllian <willian.dev10@gmail.com>
2020-04-06 10:11:19 -0300

    test: add create_user test and list_users test

--- dspace/api/tests.py
+++ dspace/api/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class UserTestCase(APITestCase):
+
+  def test_create_user(self):
+    self.post_data = {
+      'username': 'test',
+      'email': 'test@gmail.com',
+      'password': '123456'
+    }
+
+    return self.client.post('/api/user/', self.post_data, format='json')
+    self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+  def test_list_users(self):
+    response = self.client.get('/api/user/')
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+   
