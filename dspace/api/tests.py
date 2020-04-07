8a03e3553877bd6c8d4e4ce99b411bcc18ec3d96
MrWilllian <willian.dev10@gmail.com>
2020-04-07 16:00:22 -0300

    test: comment test method to create user with address

--- dspace/api/tests.py
+++ dspace/api/tests.py
@@ -19,8 +19,7 @@ class UserTestCase(APITestCase):
   def test_list_users(self):
     response = self.client.get('/api/v1/user/')
     self.assertEqual(response.status_code, status.HTTP_200_OK)
-   
-  
+
   def data_user_object(self):
     return {
       'id': 1,
@@ -28,3 +27,20 @@ class UserTestCase(APITestCase):
       'email': 'test@gmail.com',
       'password': '123456'
     }
+
+  # def test_create_user_with_address(self):
+  #   self.post_data = self.data_user_object()
+  #   self.post_data_address = {
+  #     "user_id": "1",
+  #     "country": "Brazil",
+  #     "state": "Sao Paulo",
+  #     "city": "Sao Paulo",
+  #     "place": "Rua Getulio",
+  #     "latitude": "-13.0183823",
+  #     "longitude": "-42.6983994"
+  #   }
+  #   response_user = self.client.post('/api/v1/user/', self.post_data, format='json')
+  #   response_address = self.client.post('/api/v1/address/', self.post_data, format='json')
+  #   self.assertEqual(response_user.status_code, status.HTTP_201_CREATED)
+  #   self.assertEqual(response_address.status_code, status.HTTP_201_CREATED)
+  
