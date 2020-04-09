5eddce22aef5d6e2611b1c00c7e80a952410cb02
MrWilllian <willian.dev10@gmail.com>
2020-04-09 15:38:52 -0300

    refactor: add distance url

--- dspace/dspace/urls.py
+++ dspace/dspace/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from tastypie.api import Api
 from api.resources import UserResource, AddressResource
+from api.controllers.calculate import distance
 
 v1_api = Api(api_name='v1')
 v1_api.register(UserResource())
@@ -9,5 +10,6 @@ v1_api.register(AddressResource())
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^api/', include(v1_api.urls))
+    url(r'^api/', include(v1_api.urls)),
+    url(r'^api/v1/distance/(?P<lat1>\d+\.\d+)/(?P<lon1>\d+\.\d+)/(?P<lat2>\d+\.\d+)/(?P<lon2>\d+\.\d+)$', distance)
 ]
