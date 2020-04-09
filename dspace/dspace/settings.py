6524902def16f5f376bf93c373630a53eacb274e
MrWilllian <willian.dev10@gmail.com>
2020-04-09 15:38:28 -0300

    refactor: remove media dir

--- dspace/dspace/settings.py
+++ dspace/dspace/settings.py
@@ -16,7 +16,6 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 STATIC_DIR = os.path.join(BASE_DIR,'static')
-MEDIA_DIR = os.path.join(BASE_DIR,'media')
 
 
 # Quick-start development settings - unsuitable for production
@@ -127,8 +126,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [STATIC_DIR,]
-
-MEDIA_ROOT = MEDIA_DIR
-MEDIA_URL = '/media/'
 
 LOGIN_URL = '/api/user_login/'
