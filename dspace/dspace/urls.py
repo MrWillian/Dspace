25c69a1b20585d50882a049b6df3eb506eea3cee
MrWilllian <willian.dev10@gmail.com>
2020-04-17 11:04:45 -0300

    refactor: add login url

--- dspace/dspace/urls.py
+++ dspace/dspace/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from tastypie.api import Api
 from api.resources import UserResource, AddressResource
 from api.controllers.calculate import distance
+from api.controllers.login import auth_user
 
 v1_api = Api(api_name='v1')
 v1_api.register(UserResource())
@@ -11,5 +12,6 @@ v1_api.register(AddressResource())
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include(v1_api.urls)),
-    url(r'^api/v1/distance/(?P<lat1>\d+\.\d+)/(?P<lon1>\d+\.\d+)/(?P<lat2>\d+\.\d+)/(?P<lon2>\d+\.\d+)$', distance)
+    url(r'^api/v1/distance/(?P<lat1>\d+\.\d+)/(?P<lon1>\d+\.\d+)/(?P<lat2>\d+\.\d+)/(?P<lon2>\d+\.\d+)$', distance),
+    url(r'^api/v1/login/', auth_user),
 ]
